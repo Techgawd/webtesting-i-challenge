929c7993189c16bdc6e634d3cd24e2201e86e771
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-05-14 15:39:58 -0700

    Completed MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,33 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement < 20){
+    return { ...item, enhancement: item.enhancement + 1 };
+  } else {
+    return item;
+  }
+  
 }
 
 function fail(item) {
-  return { ...item };
+  switch (true) {
+    case (item.enhancement < 15):
+    return { ...item, durability: item.durability -5};
+
+    case (item.enhancement > 16):
+    return {...item, enhancement: item.enhancement -1, durability: item.durability -10};
+    
+    case (item.enhancement >= 15):
+    return {...item, durability: item.durability -10};
+
+    default : return item;
+  }
+  
 }
 
 function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
